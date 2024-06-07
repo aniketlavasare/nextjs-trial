8d79f2aba7bab9610e06a2bd0485f98ad8994631
aavasare <aniketavasare2004@gmail.com>
2024-06-07 12:15:35 -0500

    delete added

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -33,3 +33,8 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices');
 
 }
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
